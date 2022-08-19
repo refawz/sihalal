82116515b55f61673389915daefb129f32af5362
refaw <reza.fawzian@gmail.com>
2022-08-19 23:46:07 +0700

    api login gaes

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,62 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // getExternalData('http://103.7.14.55/api/v1/data_list/10010/3881E330-F978-4806-8325-CF5DF175C9EC', {
+    //     onSuccess: function (res) {
+    //         console.log(res)
+    //     },
+    //     onXHRError: function (res) {
+    //         console.log(res)
+    //     }
+    // })
+    login()
 });
+
+function getExternalData(endpoint, args) {
+
+    var xhr = new XMLHttpRequest();
+    var response;
+
+    xhr.addEventListener('load', function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            response = JSON.parse(xhr.responseText);
+            // console.log('Response: ', response);
+            
+            args.onSuccess(response);
+        }
+    });
+    xhr.addEventListener('error', function () {
+        args.onXHRError(xhr);
+    });
+
+    xhr.open('GET', endpoint);
+    xhr.send(null);
+
+}
+
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  function login() {
+    postData('http://103.7.14.55/auth/signin', { 'userid': 'lphwalisongo@walisongo.ac.id', 'password': '12345678' })
+    .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+    });
+  }
