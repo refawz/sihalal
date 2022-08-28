0debdca55f59e0f9a094ccb9dbfda95ed01562e9
taufiqasshydiqi <taufiqasshydiqi@gmail.com>
2022-08-28 12:42:05 +0700

    Update API all bisa ex auditresult

--- js/scripts.js
+++ js/scripts.js
@@ -93,10 +93,31 @@ async function getData(url = '') {
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         // cookie: '__bpjph_ct=' + token,
-        credentials: 'same-origin', // include, *same-origin, omit
+        credentials: 'include', // include, *same-origin, omit
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + token,
+            // 'Authorization': 'Basic ' + token,
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// Example DELETE method implementation:
+async function deleteData(url = '') {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // cookie: '__bpjph_ct=' + token,
+        credentials: 'include', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Authorization': 'Basic ' + token,
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow', // manual, *follow, error
@@ -113,7 +134,26 @@ async function postData(url = '', data = {}) {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
+        credentials: 'include', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// Example PUT method implementation:
+async function editData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'include', // include, *same-origin, omit
         headers: {
             'Content-Type': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
@@ -129,7 +169,6 @@ function login() {
     postData('http://103.7.14.55/auth/signin', { 'userid': 'lphwalisongo@walisongo.ac.id', 'password': '12345678' })
         .then((data) => {
             console.log(data); // JSON data parsed by `data.json()` call
-            token = data.payload.token;
         });
 }
 
@@ -140,20 +179,162 @@ function logout() {
         });
 }
 
+//data permohonan yang masuk ke lph berdasarkan status
 function dataList() {
-    stat = '10010';
-    idLph = '3881E330-F978-4806-8325-CF5DF175C9EC';
-    url = 'http://103.7.14.55/api/v1/data_list/' + stat + '/' + idLph;
-    response = getData(url);
+    var stat = '10030'; //10010 = ajuan/dikirim ke lph, 10025=biaya/pembayaran, 10030=periksa/proses di LPH
+    var idLph = '3B81E330-F97B-48D6-B325-0F5DF175C9EC';
+    var url = 'http://103.7.14.55/api/v1/data_list/' + stat + '/' + idLph;
+    var response = getData(url);
 
     console.log(response);
 }
 
+//detail data permohonan
 function dataMohon() {
-    regId = '79629';
-    url = 'http://103.7.14.55/api/v1/reg/' + regId;
-    response = getData(url);
+    var regId = '79629'; //id registrasi
+    var url = 'http://103.7.14.55/api/v1/reg/' + regId;
+    var response = getData(url);
 
     console.log(response);
 }
 
+function updateStatus() {
+    var stat = 'periksa'; //ajuan, biaya, periksa 
+    var regId = '79632'; //id registrasi //ambil dari hasil data list
+    var idLph = '3B81E330-F97B-48D6-B325-0F5DF175C9EC';
+    var url = 'http://103.7.14.55/api/v1/data_list/updateStatus';
+    postData(url, { 'status': stat, 'reg_id': regId, 'lph_mapped_id': idLph })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+}
+
+//daftar biaya
+function biayaList() {
+    // var regId = '79629'; //id registrasi
+    var url = 'http://103.7.14.55/api/v1/costs';
+    var response = getData(url);
+
+    console.log(response);
+}
+
+//menambah daftar biaya pemeriksaan
+function biayaAdd() {
+    // var idBiaya = '3000';  
+    var idReg = '79632'; //id registrasi //ambil dari hasil data list
+    var keterangan = 'Biaya Pemeriksaan';
+    var qty = '1';
+    var harga = '350000';
+    // var total = '350000';
+    var url = 'http://103.7.14.55/api/v1/costs';
+    postData(url, { 'id_reg': idReg, 'keterangan': keterangan, 'qty': qty, 'harga': harga })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+}
+
+//melihat data konfirmasi pendaftaran
+function konfirmasiList() {
+    var idLph = '3B81E330-F97B-48D6-B325-0F5DF175C9EC';
+    var url = 'http://103.7.14.55/api/v1/invoice/' + idLph;
+    var response = getData(url);
+
+    console.log(response);
+}
+
+//melihat data jadwal audit
+function jadwalAuditList() {
+    // var idLph = '3B81E330-F97B-48D6-B325-0F5DF175C9EC';
+    var url = 'http://103.7.14.55/api/v1/audit_schedule/';
+    var response = getData(url);
+
+    console.log(response);
+}
+
+//menambah data jadwal audit
+function jadwalAuditAdd() {
+    var idReg = '79632'; //id registrasi //ambil dari hasil data list
+    var jadwalAwal = '2022-08-20';
+    var jadwalAkhir = '2022-08-23';
+    var jmlHari = '4';
+    var url = 'http://103.7.14.55/api/v1/audit_schedule';
+    postData(url, { 'id_reg': idReg, 'jadwal_awal': jadwalAwal, 'jadwal_akhir': jadwalAkhir, 'jml_hari': jmlHari })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+}
+
+//mengubah data jadwal audit
+function jadwalAuditUpdate() {
+    var idAudit = '4063';
+    var idReg = '79632'; //id registrasi //ambil dari hasil data list
+    var jadwalAwal = '2022-08-21';
+    var jadwalAkhir = '2022-08-23';
+    var jmlHari = '3';
+    var url = 'http://103.7.14.55/api/v1/audit_schedule/' + idAudit;
+    editData(url, { 'id_reg': idReg, 'jadwal_awal': jadwalAwal, 'jadwal_akhir': jadwalAkhir, 'jml_hari': jmlHari })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+}
+
+//menghapus data jadwal audit
+function jadwalAuditDelete() {
+    var idAudit = '4063';
+    var url = 'http://103.7.14.55/api/v1/audit_schedule/' + idAudit;
+    var response = deleteData(url);
+
+    console.log(response);
+}
+
+//menampilkan list data seluruh auditor
+function periksaListAuditor() {
+    var url = 'http://103.7.14.55/api/v1/reg_auditor';
+    var response = getData(url);
+
+    console.log(response);
+}
+
+//menambahkan auditor
+function periksaAuditorAdd() {
+    var idReg = '79632'; //id registrasi //ambil dari hasil data list
+    var auditorId = 'F0D51FC3-8D7D-418F-9CC9-213606753069';
+    var createBy = 'BBSPJIT';
+    var url = 'http://103.7.14.55/api/v1/reg_auditor';
+    postData(url, { 'id_reg': idReg, 'auditor_id': auditorId, 'create_by': createBy })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+}
+
+//menghapus auditor
+function periksaAuditorDelete() {
+    var idReg = '79632';
+    var url = 'http://103.7.14.55/api/v1/reg_auditor/' + idReg;
+    var response = deleteData(url);
+
+    console.log(response);
+}
+
+//menampilkan list data auditor LPH
+function checkAuditorList() {
+    var idLph = '3B81E330-F97B-48D6-B325-0F5DF175C9EC'; //id lph??
+    var url = 'http://103.7.14.55/api/v1/check_auditor_list/' + idLph;
+    var response = getData(url);
+
+    console.log(response);
+}
+
+//menambahkan auditor
+function auditResult() {
+    var idReg = '79632'; //id registrasi //ambil dari hasil data list
+
+    var keterangan = 'okeeyyyy';
+    var tglSelesai = '2022-08-23';
+    var hasilAudit = 'PR001';
+    var url = 'http://103.7.14.55/api/v1/audit_result';
+    postData(url, { 'id_reg': idReg, 'keterangan': keterangan, 'tgl_selesai': tglSelesai, 'hasil_audit': hasilAudit })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+}
